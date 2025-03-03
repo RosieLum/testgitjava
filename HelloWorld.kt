faee3c4fceae0298fba8771872b513ec76486cd3
RosieLum <huonggiang.hust@gmail.com>
2025-03-03 21:38:05 +0700

    commit

---
+++ HelloWorld.kt
@@ -0,0 +1,26 @@
+object HelloWorld {
+    @kotlin.jvm.JvmStatic
+    fun main(args: Array<String>) {
+//        System.out.println("Hello");
+//        int lum1 = 52;
+//        short lum2 = 67;
+//        char lum3 = 'C';
+//        String lum4 = "Hello world";
+//        boolean lum5 = true;
+//        float lum6 = 6347.37f;
+//        double lum7 = 436.32682;
+//        byte lum8 = -5;
+//        System.out.println(lum4);
+
+//        int lum1 = 6;
+//        int lum2 = 9;
+//        double lum3 = (double) lum1/lum2;
+//        System.out.println(lum3);
+
+        var a = 7
+        val sum = a-- + ++a - --a - a++ + a--
+        //        =7 + 7 - 6 - 6 + 6
+//        System.out.println(sum);
+        println(sum)
+    }
+}
